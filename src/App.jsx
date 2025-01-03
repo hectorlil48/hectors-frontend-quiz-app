dbe5e0daae94952b6520ed7275cfe6a7372e7c4e
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2025-01-02 20:05:34 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Hello from "./Hello";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Hello />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Hello.jsx
@@ -0,0 +1,5 @@
+function Hello() {
+  return <h2>Im from the Hello file!</h2>;
+}
+
+export default Hello;
