1ba7d7d8ea3541ddc92d020237003d7dc905442f
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2025-01-27 20:58:49 -0600

    Started to add styles for light and dark mode.

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <NavBar />
       <h1>Hello World!</h1>
-    </>
+    </div>
   );
 }
 
