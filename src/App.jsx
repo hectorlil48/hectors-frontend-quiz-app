810edfddd759d11fdc65dd84c0d021d41cad8926
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2025-01-28 17:42:02 -0600

    Moved some images into public, started working on the navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import NavBar from "./components/NavBar";
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <h1>Hello World!</h1>
+      <div className="container">
+        <NavBar />
+      </div>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,17 @@
 const NavBar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div>
+        <img src="" alt="question category" />
+        <h1>Accessibility</h1>
+      </div>
+      <div>
+        <img src="" alt="sun icon" />
+        <div></div>
+        <img src="" alt="moon icon" />
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
