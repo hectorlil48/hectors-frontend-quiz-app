7a634cd7d74bed9022e83071ea7f7ae43cb20d0d
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2025-01-28 19:00:50 -0600

    Styled my navbar, I hide my check box and added a switch.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,34 @@
+import accessibilityIcon from "../assets/images/icon-accessibility.svg";
+import sunIcon from "../assets/images/icon-sun-dark.svg";
+import moonIcon from "../assets/images/icon-moon-dark.svg";
+import "../styles/NavBar.css";
+
 const NavBar = () => {
   return (
-    <nav>
-      <div>
-        <img src="" alt="question category" />
+    <nav className="navbar flex">
+      <div className="logo-icon flex">
+        <img
+          src={accessibilityIcon}
+          alt="question category"
+          className="title-icon"
+        />
         <h1>Accessibility</h1>
       </div>
-      <div>
-        <img src="" alt="sun icon" />
-        <div></div>
-        <img src="" alt="moon icon" />
+      <div className="toggle-container flex">
+        <img src={sunIcon} alt="sun icon" />
+
+        <div className="toggle-box flex">
+          <input
+            type="checkbox"
+            id="check"
+            className="toggle"
+            // onChange={handleChange}
+            // checked={isChecked}
+          />
+          <label htmlFor="check"></label>
+        </div>
+
+        <img src={moonIcon} alt="moon icon" />
       </div>
     </nav>
   );
